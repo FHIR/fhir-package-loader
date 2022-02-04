0a4036e38ef8e1cbfce39710bc53234adcc33e40
Julia Afeltra <jafeltra@mitre.org>
2022-02-04 17:10:13 -0500

    Update CLI options

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,17 @@
 
 import { program } from 'commander';
 import { loadDependencies } from './loadUtils';
+import { logger } from './utils';
 
 async function myApp() {
   program
-    .name('fhir-package-loader')
+    .name('fhir-package-load')
     .usage('<fhirPackages...> [options]')
-    .option('-s, --save <dir>', 'where to save packages to (default is the FHIR cache)')
+    .option(
+      '-s, --save <dir>',
+      'where to save packages to and load definitions from (default is the local FHIR cache)'
+    )
+    .option('-d, --debug', 'output extra debugging information')
     .argument(
       '<fhirPackages...>',
       'list of FHIR packages to load using the format packageId@packageVersion...'
@@ -16,11 +21,13 @@ async function myApp() {
       'after',
       `
 Examples:
-  npx fhir-package-loader hl7.fhir.r5.core@current
-  fhir-package-loader hl7.fhir.r4.core@4.0.1 hl7.fhir.us.core@4.0.0 --save ./myProject`
+  npx fhir-package-load hl7.fhir.r5.core@current
+  fhir-package-load hl7.fhir.r4.core@4.0.1 hl7.fhir.us.core@4.0.0 --save ./myProject`
     )
-    .version('0.1.0')
+    .version('0.1.0', '-v, --version') // Use -v (instead of default -V)
     .parse(process.argv);
+
+  if (program.opts().debug) logger.level = 'debug';
 
   const packages = program.args.map(dep => dep.replace('@', '#'));
   const cachePath = program.opts().save;
