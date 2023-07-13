2b59a31682443905280984131c2923b69a86b706
Julia Afeltra <jafeltra@mitre.org>
2023-07-13 10:24:57 -0400

    Update error parameter name

--- src/errors/LatestVersionUnavailableError.ts
+++ src/errors/LatestVersionUnavailableError.ts
@@ -2,11 +2,11 @@ export class LatestVersionUnavailableError extends Error {
   constructor(
     public packageName: string,
     public customRegistry?: string,
-    public isPatch?: boolean
+    public isPatchWildCard?: boolean
   ) {
     super(
       `Latest ${
-        isPatch ? 'patch ' : ''
+        isPatchWildCard ? 'patch ' : ''
       }version of package ${packageName} could not be determined from the ${
         customRegistry ? 'custom ' : ''
       }FHIR package registry${customRegistry ? ` ${customRegistry}` : ''}`
